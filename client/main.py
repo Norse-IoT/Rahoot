db01cb914149892c718309d4a5ef1c290e244d2f
Zack Sargent <zack@zack.fyi>
2025-01-20 14:12:57 -0500

    use version that can score points

--- client/main.py
+++ client/main.py
@@ -22,8 +22,17 @@ def disconnect():
     print("disconnected from server")
 
 
+def background_task():
+    print("background task")
+
+    while True:
+        answer = input("What choice do you want? (0-3)")
+        sio.emit("player:selectedAnswer", int(answer))
+
+
 ROOM_NUMBER = 621639
 
 sio.connect("http://localhost:5505")
 sio.emit("player:join", {"username": "python", "room": str(ROOM_NUMBER)})
+sio.start_background_task(background_task)
 sio.wait()
