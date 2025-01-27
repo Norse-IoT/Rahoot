6116e0d08325d42a9af3075085786e856b403c07
Zack Sargent <zack@zack.fyi>
2025-01-26 21:26:02 -0500

    update script

--- start.sh
+++ start.sh
@@ -4,5 +4,10 @@ set -e # stop immediately on errors
 set -o pipefail # do not silently ignore errors in pipelines
 cd "${0%/*}" # cd into script's current location
 
+if [ -z "${NVM_DIR}" ]; then
+  # Set the backup value if the variable is not set
+  NVM_DIR="$HOME/.nvm"
+fi
+
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 npm run all
