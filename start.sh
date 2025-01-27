9a1a7aa64faf9c5fcf11e0d0f242a6b5e7ca25d2
Zack Sargent <zack@zack.fyi>
2025-01-26 21:22:18 -0500

    add start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e # stop immediately on errors
+set -o pipefail # do not silently ignore errors in pipelines
+cd "${0%/*}" # cd into script's current location
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+npm start
